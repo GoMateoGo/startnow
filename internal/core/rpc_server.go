55871f93fa1a55e270f8df68dcefa2031db7e8a9
mateo <mrmateoliu@gmail.com>
2025-02-07 00:42:27 +0800

    feat: 健康检测,未完成.

--- internal/core/rpc_server.go
+++ internal/core/rpc_server.go
@@ -7,8 +7,10 @@ import (
 	"second_hand_mall/internal/global"
 	"second_hand_mall/router"
 	"sync"
+	"time"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/keepalive"
 )
 
 func RunRpcServer(wg *sync.WaitGroup) {
@@ -17,7 +19,13 @@ func RunRpcServer(wg *sync.WaitGroup) {
 	if err != nil {
 		log.Fatalf("监听tcp失败: %v", err)
 	}
-	s := grpc.NewServer()
+	s := grpc.NewServer(grpc.KeepaliveParams(keepalive.ServerParameters{
+		MaxConnectionIdle: 5 * time.Minute,  // 连接保持空闲的最大时间
+		Time:              30 * time.Second, // 服务器发送保持活动（keepalive）探测的时间间隔。服务器会定期发送探测消息以检查连接是否仍然有效。
+		Timeout:           10 * time.Second, // 服务器等待保持活动探测响应的超时时间。如果服务器在发送探测消息后，在 Timeout 时间内没有收到响应，它会认为连接已经断开，并关闭连接。
+		//MaxConnectionAge:      30 * time.Minute, // 一个连接存在的最大时间
+		//MaxConnectionAgeGrace: 5 * time.Minute, // 当超过最大时间时,给与正在进行请求的宽限时间
+	}))
 	// 注册Rpc服务
 	router.RegisterRpcMethod(s)
 	//pb.RegisterGreeterServer(s, &rpc_server{})
--- rpc_method/db_demo/client/main.go
+++ rpc_method/db_demo/client/main.go
@@ -4,41 +4,107 @@ import (
 	"context"
 	"log"
 	dbhandler "second_hand_mall/rpc_method/db_demo"
+	"sync"
 
 	"time"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/connectivity"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/keepalive"
+)
+
+var (
+	conn   *grpc.ClientConn
+	client dbhandler.DBServiceClient
 )
 
 func main() {
-	conn, err := grpc.NewClient("localhost:5001", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	defer conn.Close()
+	var err error
+	conn, err = grpc.NewClient("localhost:5001",
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithKeepaliveParams(keepalive.ClientParameters{
+			Time:                20 * time.Second, // 每隔 30 秒发送心跳
+			Timeout:             10 * time.Second, // 等待确认的超时时间
+			PermitWithoutStream: true,             // 无活跃流时仍发送心跳
+		}),
+		grpc.WithDefaultServiceConfig(`{
+        "methodConfig": [{
+            "name": [{"service": "dbhandler.DBService"}],
+            "retryPolicy": {
+                "MaxAttempts": 3,
+                "InitialBackoff": "0.1s",
+                "MaxBackoff": "1s",
+                "BackoffMultiplier": 2.0,
+                "RetryableStatusCodes": ["UNAVAILABLE"]
+            }
+        }]
+    }`))
 	if err != nil {
 		log.Fatalf("连接失败:%v", err)
 	}
-	defer conn.Close()
-	c := dbhandler.NewDBServiceClient(conn)
-	//Get(c)
-	List(c)
+
+	client = dbhandler.NewDBServiceClient(conn)
+
+	var wg sync.WaitGroup
+
+	// 初始化 ticker，每1秒触发一次
+	ticker := time.NewTicker(1 * time.Second)
+	defer ticker.Stop()
+
+	// 启动健康检查
+	wg.Add(1)
+	go checkHealth(&wg, ticker)
+	go func() {
+		for {
+			time.Sleep(30 * time.Second)
+			Get()
+			List()
+		}
+	}()
+	wg.Wait()
+}
+func checkHealth(wg *sync.WaitGroup, ticker *time.Ticker) {
+	defer wg.Done()
+
+	for {
+		select {
+		case <-ticker.C:
+			state := conn.GetState()
+			if state != connectivity.Ready && state != connectivity.Idle {
+				log.Println("连接不健康，尝试重置...")
+				conn.ResetConnectBackoff()
+			} else {
+				log.Println("rpc client 连接健康", state)
+			}
+		case <-time.After(5 * time.Second):
+			// 设置超时，防止 ticker 阻塞
+			log.Println("检测超时")
+			return
+		}
+	}
 }
 
-func Get(c dbhandler.DBServiceClient) {
+func Get() {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
-	r, err := c.Get(ctx, &dbhandler.GetRequest{Cid: 0, Id: 3})
+	r, err := client.Get(ctx, &dbhandler.GetRequest{Cid: 0, Id: 3})
 	if err != nil {
-		log.Fatalf("请求错误:%v", err)
+		log.Printf("请求错误:%v", err)
+		return
 	}
 
 	log.Println(r.Id, r.Name)
 }
 
-func List(c dbhandler.DBServiceClient) {
+func List() {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
-	r, err := c.List(ctx, &dbhandler.ListRequest{Cid: 2008})
+	r, err := client.List(ctx, &dbhandler.ListRequest{Cid: 2008})
 	if err != nil {
-		log.Fatalf("请求错误:%v", err)
+		log.Printf("请求错误:%v", err)
+		return
 	}
 
 	log.Println(r.GetData())
